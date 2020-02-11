817a76207d95f15c1f8b6060f0fd1a45b1a3bf60
edipojuan <edipojs@gmail.com>
2020-02-11 18:12:23 -0400

    Added requests

--- src/app.py
+++ src/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello, Testtt123!"
+    response = requests.get(
+        'https://api.github.com/users/edipojuan', verify=False)
+    if response.status_code == 200:
+        return response.text
+    elif response.status_code == 404:
+        return "Nenhum valor encontrado."
 
 
 @app.route("/test")
